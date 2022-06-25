c711a860b017505a0157a6212f8a660c9d69b181
Clemens Drüe <druee@uni-trier.de>
2022-06-25 14:23:06 +0200

    added skeleton files

--- setup.py
+++ setup.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
-import os
-
-from setuptools import setup
-
-v = {}
-v_path = os.path.join(*'meteolib/_version.py'.split('/'))
-with open(v_path) as v_file:
-    exec(v_file.read(), v)
 
 setup(
-    name=v['__title__'],
-    version=v['__version__'],
-    packages=[v['__title__']],
-    package_dir={v['__title__']: v['__title__']},
-    license=v['__license__'],
-    author=v['__author__'],
-    author_email=v['__author_email__'],
-    url=v['__url__'],
-    description=v['__description__'],
-    long_description=open('README.md').read(),
-    install_requires=[
-        'numpy',
-        'pandas',
-    ],
+    name='meteolib',
+    version='0.0.0',
+    license='EUPL-1.1',
+    author="Clemens Drüe",
+    author_email='druee@uni-trier.de',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/cdruee/meteolib',
     keywords='meteorology',
+    install_requires=[
+          'numpy', 'pandas'
+      ],
+
 )
